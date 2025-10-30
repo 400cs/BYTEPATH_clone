7df866f2f13bce8fff7640027794515d731b8417
400cs <78524100+400cs@users.noreply.github.com>
2025-10-30 02:03:52 -0700

    added Moses (a tables lib)

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 Object = require 'libraries/classic/classic' --global var for class library
 Input = require 'libraries/boipushy/Input' --global var for input library
 Timer = require 'libraries/chrono/Timer' --global var for timer library
+Moses = require 'libraries/Moses/moses' --global var for moses library
 
 
 function recursiveEnumerate(folder, file_list)
